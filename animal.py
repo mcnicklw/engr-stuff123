cc21feaac917e2b4751d77133d3c9f3a9c115307
mcnic <mcnicklw@oregonstate.edu>
2024-06-26 23:21:13 -0700

    no idea
    Signed-off-by: mcnic <mcnicklw@oregonstate.edu>

---
+++ animal.py
@@ -0,0 +1,6 @@
+# Author: Wyatt McNickle
+# GitHub username: mcnicklw
+# Date: 6/26/2024
+# Description: Asks the user for their favorite animal then prints out that that is their favorite animal
+fave_animal = input("Please enter your favorite animal:")
+print("your favorite animal is the " + fave_animal + ".")
