bcaefa9bfb6c1890284a3fa47befcf2c445f208d
mcnic <mcnicklw@oregonstate.edu>
2024-07-31 21:53:23 -0700

    he
    Signed-off-by: mcnic <mcnicklw@oregonstate.edu>

--- square_list.py
+++ square_list.py
@@ -2,6 +2,6 @@ def square_list(nums):
   for i in range(len(nums)):
     nums[i]= nums[i]**2
 
-num_list = [2, 4, 5, 6, 7]
+##num_list = [2, 4, 5, 6, 7]
 square_list(num_list)
-print(num_list)
+##print(num_list)
